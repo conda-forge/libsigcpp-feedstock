cf2622cae6694e32e225cd7469718cc4800877c6
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 14:20:04 -0400

    just re-exp

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,11 +12,11 @@ if [ "$(uname)" == "Darwin" ]; then
   #export CC=clang
   #export CXX=clang++
   #export MACOSX_VERSION_MIN="10.7"
-  #export MACOSX_VERSION_MIN="10.9"
+  export MACOSX_VERSION_MIN="10.9"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
+  #export LDFLAGS="${LDFLAGS} -stdlib=libc++"
   export LINKFLAGS="${LDFLAGS}"
 fi
 #export PKG_CONFIG_PATH=
