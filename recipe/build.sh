277471549a4aedd05076b6c08b6cebf1e23da067
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 14:14:33 -0400

    rm version

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ if [ "$(uname)" == "Darwin" ]; then
   #export CC=clang
   #export CXX=clang++
   #export MACOSX_VERSION_MIN="10.7"
-  export MACOSX_VERSION_MIN="10.9"
+  #export MACOSX_VERSION_MIN="10.9"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
