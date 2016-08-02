6221432acc3522997ba9abb51fbfc2a96501ed35
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 12:23:43 -0400

    set to clang

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,13 +9,13 @@ set -e
 #export INCLUDE_PATH="${PREFIX}/include"
 if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
-#  export CC=clang
-#  export CXX=clang++
+  export CC=clang
+  export CXX=clang++
 #  export MACOSX_VERSION_MIN="10.7"
 #  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+#  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
 #  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
+#  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
 #  export LINKFLAGS="${LDFLAGS}"
 fi
 #export PKG_CONFIG_PATH=
