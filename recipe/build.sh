edbbf2f4b723bb3c8dd66e11de1b02832fcc5faa
Anthony Scopatz <scopatz@gmail.com>
2016-08-01 20:44:34 -0400

    toolchain update

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,23 @@
 set -e
 
 # set and verify flags
-export LDFLAGS=
-export CFLAGS="-O3"
-export CXXFLAGS="-O3"
-export LIBRARY_PATH="${PREFIX}/lib"
-export INCLUDE_PATH="${PREFIX}/include"
-if [ "$(uname)" == "Darwin" ]; then
+#export LDFLAGS=
+#export CFLAGS="-O3"
+#export CXXFLAGS="-O3"
+#export LIBRARY_PATH="${PREFIX}/lib"
+#export INCLUDE_PATH="${PREFIX}/include"
+#if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.7"
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
-  export LINKFLAGS="${LDFLAGS}"
-fi
-export PKG_CONFIG_PATH=
+#  export CC=clang
+#  export CXX=clang++
+#  export MACOSX_VERSION_MIN="10.7"
+#  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+#  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+#  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+#  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+#  export LINKFLAGS="${LDFLAGS}"
+#fi
+#export PKG_CONFIG_PATH=
 
 # configure, make, install, check
 ./configure --prefix="${PREFIX}" \
