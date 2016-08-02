965ce5673d0596ab3df6b49f709cfa2a816e7dcb
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 13:30:52 -0400

    only low ver

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,14 +9,14 @@ set -e
 #export INCLUDE_PATH="${PREFIX}/include"
 if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
-  export CC=clang
-  export CXX=clang++
+  #export CC=clang
+  #export CXX=clang++
   export MACOSX_VERSION_MIN="10.7"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
-  export LINKFLAGS="${LDFLAGS}"
+  #export LDFLAGS="${LDFLAGS} -stdlib=libc++"
+  #export LINKFLAGS="${LDFLAGS}"
 fi
 #export PKG_CONFIG_PATH=
 
