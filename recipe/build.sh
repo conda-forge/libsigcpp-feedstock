df2cfeec130e7aa761c9afb2b967f31767e22727
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 12:03:29 -0400

    add link flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,17 +7,17 @@ set -e
 #export CXXFLAGS="-O3"
 #export LIBRARY_PATH="${PREFIX}/lib"
 #export INCLUDE_PATH="${PREFIX}/include"
-#if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
 #  export CC=clang
 #  export CXX=clang++
 #  export MACOSX_VERSION_MIN="10.7"
 #  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-#  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
 #  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-#  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
 #  export LINKFLAGS="${LDFLAGS}"
-#fi
+fi
 #export PKG_CONFIG_PATH=
 
 # configure, make, install, check
