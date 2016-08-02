f7d9acfce18965ca69f37c2ad2aa93141a63c76f
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 14:28:16 -0400

    no clue why this works

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,24 +2,14 @@
 set -e
 
 # set and verify flags
-#export LDFLAGS=
-#export CFLAGS="-O3"
-#export CXXFLAGS="-O3"
-#export LIBRARY_PATH="${PREFIX}/lib"
-#export INCLUDE_PATH="${PREFIX}/include"
 if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
-  #export CC=clang
-  #export CXX=clang++
-  #export MACOSX_VERSION_MIN="10.7"
+  # It seems like this is/should be done by toolchain...
   export MACOSX_VERSION_MIN="10.9"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  #export LDFLAGS="${LDFLAGS} -stdlib=libc++"
   export LINKFLAGS="${LDFLAGS}"
 fi
-#export PKG_CONFIG_PATH=
 
 # configure, make, install, check
 ./configure --prefix="${PREFIX}" \
@@ -27,7 +17,3 @@ fi
 make
 make check
 make install
-
-#            --enable-static \
-#            --with-libsigc-includes="${PREFIX}/include" \
-#            --with-libsigc-libraries="${PREFIX}/lib" \
