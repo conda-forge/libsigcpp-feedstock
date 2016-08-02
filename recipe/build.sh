860a208fd2d80ef41898ae8102984aec8835f0e8
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 12:31:53 -0400

    more

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,21 +7,23 @@ set -e
 #export CXXFLAGS="-O3"
 #export LIBRARY_PATH="${PREFIX}/lib"
 #export INCLUDE_PATH="${PREFIX}/include"
-if [ "$(uname)" == "Darwin" ]; then
+#if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
-  export CC=clang
-  export CXX=clang++
+#  export CC=clang
+#  export CXX=clang++
 #  export MACOSX_VERSION_MIN="10.7"
 #  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
 #  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
 #  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
 #  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
 #  export LINKFLAGS="${LDFLAGS}"
-fi
+#fi
 #export PKG_CONFIG_PATH=
 
 # configure, make, install, check
 ./configure --prefix="${PREFIX}" \
+            --enable-static \
+            --disable-rpath \
   || { cat config.log; exit 1; }
 make
 make check
