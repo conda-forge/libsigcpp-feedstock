b41f7b85a15949b48186b2cee1fa9726e2e694ac
jakirkham <jakirkham@gmail.com>
2022-01-04 14:12:41 -0800

    Update `config.*` for macOS ARM

--- recipe/build_libsigcpp.sh
+++ recipe/build_libsigcpp.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./etc
+
 if [[ "$target_platform" == osx* ]]; then
   # C++17 support
   export CXXFLAGS="${CXXFLAGS} -std=c++1z"
