383034e53ee8cb6d27ebf664d7b289d6167609cf
Isuru Fernando <isuruf@gmail.com>
2022-01-06 09:39:12 +0530

    Update build_libsigcpp.sh

--- recipe/build_libsigcpp.sh
+++ recipe/build_libsigcpp.sh
@@ -6,14 +6,9 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./build
 
 mkdir builddir
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-    meson ${MESON_ARGS} builddir .
-else
-    meson --prefix="${PREFIX}" --libdir=lib builddir .
-fi
+meson ${MESON_ARGS} --prefix="${PREFIX}" --libdir=lib builddir .
 
 cd builddir
 ninja -j${CPU_COUNT}
 ninja test
 ninja install
-#
