a729556014f2149af925c8ff902ded05483ceb52
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2022-08-28 07:07:40 +0530

    Bump to 3.2.0 and cleanup

--- recipe/build_libsigcpp.sh
+++ recipe/build_libsigcpp.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build
+meson ${MESON_ARGS} --prefix="${PREFIX}" -Dlibdir=lib builddir .
 
-if [[ "$target_platform" == osx* ]]; then
-  # C++17 support
-  export CXXFLAGS="${CXXFLAGS} -std=c++1z"
-fi
-
-# configure, make, install, check
-./configure --prefix="${PREFIX}" \
-  || { cat config.log; exit 1; }
-
-make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  make check
-fi
-make install
+ninja -C builddir -j${CPU_COUNT}
+ninja -C builddir test
+ninja -C builddir install
